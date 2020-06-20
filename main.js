682d53b4fb5ffc0a96dc27509d0993035a6c33e2
John! <jhonmvl18@gmail.com>
2020-06-19 22:06:21 -0500

    Final 1

--- main.js
+++ main.js
@@ -27,12 +27,12 @@ function fetchData() {
 function showFoodMenu() {
   $.ajax({
     type: "GET",
-    url: "./demo.xml",
+    url: "./gym.xml",
     dataType: "xml",
 
     error: function (e) {
-      alert("An error occurred while processing XML file");
-      console.log("XML reading Failed: ", e);
+      alert("A ocurrido un error procesando el archivo XML");
+      console.log("Lectura de XML fallida: ", e);
     },
 
     success: function (response) {
@@ -41,38 +41,50 @@ function showFoodMenu() {
       $("ul").children().remove();
 
       $(response)
-        .find("food")
+        .find("gimnasio")
         .each(function () {
-          var _name = $(this).find("name").text();
-          console.log(_name);
+          var _nombre = $(this).find("nombre").text();
+          console.log(_nombre);
 
-          var _price = $(this).find("price").text();
-          var _calories = $(this).find("calories").text();
-          var _description = $(this).find("description").text();
+          var _precio = $(this).find("precio").text();
+          var _direccion = $(this).find("direccion").text();
+          var _horario = $(this).find("horario").text();
+          var _actividades = $(this).find("actividades").text();
 
           // add content to the HTML
           $("ul").append(
             "<li>" +
-              `<font color="red" face="Comic Sans MS,arial,verdana">Nombre: </font>` +
-              _name +
+              `<font color="red" face="Comic Sans MS,arial,verdana">Nombre: </font>
+               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;` +
+              _nombre +
               "</li>"
           );
           $("ul").append(
             "<li>" +
-              `<font color="red" face="Comic Sans MS,arial,verdana">Precio: </font>` +
-              _price +
+              `<font color="red" face="Comic Sans MS,arial,verdana">Precio: </font>
+               &nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp; <span> $ </span> ` +
+              _precio +
               "</li>"
           );
           $("ul").append(
             "<li>" +
-              `<font color="red" face="Comic Sans MS,arial,verdana">Calorias: </font>` +
-              _calories +
+              `<font color="red" face="Comic Sans MS,arial,verdana">Dirección: </font> 
+              &nbsp&nbsp;&nbsp;&nbsp;` +
+              _direccion +
               "</li>"
           );
           $("ul").append(
             "<li>" +
-              `<font color="red" face="Comic Sans MS,arial,verdana">Descripcion: </font>` +
-              _description +
+              `<font color="red" face="Comic Sans MS,arial,verdana">Horario: </font>
+               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;` +
+              _horario +
+              "</li>"
+          );
+          $("ul").append(
+            "<li>" +
+              `<font color="red" face="Comic Sans MS,arial,verdana">Actividades: </font>
+               &nbsp;` +
+              _actividades +
               "</li>"
           );
           $("ul").append("<br>");
